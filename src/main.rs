0d5d213526cafbc07809bf9a3c3e67024c7e7537
chris <chris@indefini.org>
2018-04-10 17:03:09 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate enet_sys as enet;
 use std::ffi::CString;
+use std::ptr;
 
 fn main() {
     if unsafe { enet::enet_initialize() } != 0 {
@@ -9,11 +10,14 @@ fn main() {
     println!("yop la client");
     unsafe { 
         let c = enet::client_new();
-        let address = CString::new("localhost".as_bytes()).unwrap().as_ptr();
-        let p = enet::client_connect(c, address, 1234);
+        let address = CString::new("localhost".as_bytes()).unwrap();
+        let p = enet::client_connect(c, address.as_ptr(), 1234);
             
         enet::handle_event(c);
-        enet::host_send(c,p);
+
+        if p != ptr::null() {
+            enet::host_send(c,p);
+        }
 
         loop {
             enet::handle_event(c);
